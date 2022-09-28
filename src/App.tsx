cc863ef66dba1ca5c27f93a39e2e6ef767ac3a72
Jenil <jenilghevariya@crystaltechnolab.com>
2022-09-28 12:15:35 +0530

    feat: 🎸 Added tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div className="App">UI IS COMING SOON. </div>;
+  return (
+    <div className="bg-red-800 text-white flex justify-center items-center font-bold text-9xl w-full h-screen">
+      UI IS COMING SOON.
+    </div>
+  );
 }
 
 export default App;
