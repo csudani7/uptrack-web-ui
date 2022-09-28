3da4d11786f6f0fd646d535502fad56501fe3b3b
Jenil <jenilghevariya@crystaltechnolab.com>
2022-09-28 12:09:37 +0530

    feat: 🎸 Added husky, git-cz and lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div className="App">UI IS COMING SOON.</div>;
+  return <div className="App">UI IS COMING SOON. </div>;
 }
 
 export default App;
